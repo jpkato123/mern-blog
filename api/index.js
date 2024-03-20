2d17136cc8e151cd3bdc15e7fe17fb65180d6452
jpkato <quanghieu.forever@gmail.com>
2024-03-20 23:20:04 +0900

    create mongodb and connected !

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
+import { configDotenv } from 'dotenv';
+import e from 'express';
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+//process.env.MONGO
+dotenv.config();
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log('mongodb is connected')
+})
+.catch(err =>{
+console.log(err)
+});
 const app = express();
 
 
