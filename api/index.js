6d6ef7d0a5df15c86b463a48efb41e61e29da40b
jpkato <quanghieu.forever@gmail.com>
2024-03-21 21:53:48 +0900

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 //process.env.REACT_APP_MONGO chua chay
 dotenv.config();
@@ -16,10 +17,11 @@ console.log(err)
 });
 const app = express();
 
-
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('server is running on port 3000')
 });
 
 
 app.use('/api/user',userRoutes); // --> api/user/test
+app.use('/api/auth',authRoutes); //signup api
