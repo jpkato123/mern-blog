7aa9f9c89689a34c7c335e2dd50b7ecc95482fd2
jpkato <quanghieu.forever@gmail.com>
2024-04-02 22:04:31 +0900

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 //process.env.REACT_APP_MONGO chua chay
@@ -29,6 +30,7 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRoutes); // --> api/user/test
 app.use('/api/auth',authRoutes); //signup api: /api/auth/signup
+app.use('/api/post',postRoutes);
 
 //middleware
 app.use((err,req,res,next)=>{
