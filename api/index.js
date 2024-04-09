66f03c01aa43289bafa1043402cfcf906b77ed30
jpkato <quanghieu.forever@gmail.com>
2024-04-09 21:17:53 +0900

    add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 
 //process.env.REACT_APP_MONGO chua chay
@@ -31,6 +32,7 @@ app.listen(3000,()=>{
 app.use('/api/user',userRoutes); // --> api/user/test
 app.use('/api/auth',authRoutes); //signup api: /api/auth/signup
 app.use('/api/post',postRoutes);
+app.use("/api/comment", commentRoutes);
 
 //middleware
 app.use((err,req,res,next)=>{
