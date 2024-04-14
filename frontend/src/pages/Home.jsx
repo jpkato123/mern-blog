2e0405e11758ebe86fdb245970c996ca70237410
jpkato <quanghieu.forever@gmail.com>
2024-04-14 14:38:54 +0900

    complete the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,29 +1,51 @@
+import {Link} from 'react-router-dom'
+import CallToAction from "../components/CallToAction";
+import PostCard from '../components/PostCard'
+import { useEffect, useState } from 'react';
+
 const Home = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(()=>{
+    const fetchPost = async () => {
+      const res = await fetch('/api/post/getposts');
+      const data = await res.json()
+      if(res.ok) {
+        setPosts(data.posts)
+      }
+    }
+    fetchPost();
+  },[])
   return (
-    <div className="w-full m-auto">
-      <div className="grid grid-rows-4 grid-flow-col gap-4 justify-items-center">
-        <div className="w-64 h-10 text-white bg-black text-center justify-items-center">
-          1
-        </div>
-        <div className="w-64 h-10 text-white bg-black">2</div>
-        <div className="w-64 h-10 text-white bg-black">3</div>
-        <div className="w-64 h-10 text-white bg-black">4</div>
-        <div className="w-64 h-10 text-white bg-black">5</div>
-        <div className="w-64 h-10 text-white bg-black">6</div>
-        <div className="w-64 h-10 text-white bg-black">7</div>
-        <div className="w-64 h-10 text-white bg-black">8</div>
-        <div className="w-64 h-10 text-white bg-black">9</div>
+    <div className="">
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Wellcome to my blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          here you'll find a variety of articles and tutorials on topics such as
+          web development, software engineering, and programming languages
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold"
+        >
+          View all posts
+        </Link>
       </div>
-      <div className="grid grid-cols-1 md:grid-rows-4 grid-flow-col gap-4">
-        <div className="w-64 h-10 text-white bg-black">1</div>
-        <div className="w-64 h-10 text-white bg-black">2</div>
-        <div className="w-64 h-10 text-white bg-black">3</div>
-        <div className="w-64 h-10 text-white bg-black">4</div>
-        <div className="w-64 h-10 text-white bg-black">5</div>
-        <div className="w-64 h-10 text-white bg-black">6</div>
-        <div className="w-64 h-10 text-white bg-black">7</div>
-        <div className="w-64 h-10 text-white bg-black">8</div>
-        <div className="w-64 h-10 text-white bg-black">9</div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className=" max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className=' text-2xl font-semibold text-center'>Recent Post</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post}/>
+              ))}
+            </div>
+            <Link to='/search' className='text-lg text-teal-500 hover:underline text-center'>View all post</Link>
+          </div>
+        )}
       </div>
     </div>
   );
